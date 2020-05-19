62b4c64e97e1bd24dbedf28996b843ba2de757cc
Avinoam Sebbah <45938118+AvinoamSebbah@users.noreply.github.com>
2020-05-19 22:58:57 +0300

    Update Science.sql

--- ScienceDepartment/Science.sql
+++ ScienceDepartment/Science.sql
@@ -1,31 +1,60 @@
+CREATE TABLE laboratory
+(
+  labLocation VARCHAR(60) NOT NULL,
+  protectionLevel INT NOT NULL,
+  labName VARCHAR(60) NOT NULL,
+  labId INT NOT NULL,
+  PRIMARY KEY (labId)
+);
+
 CREATE TABLE scientist
 (
   scientistId INT NOT NULL,
-  scientistName VARCHAR(20) NOT NULL,
+  scientistName VARCHAR(60) NOT NULL,
   age INT NOT NULL,
-  salary FLOAT NOT NULL,
-  specialization VARCHAR(25) NOT NULL,
+  salary INT NOT NULL,
+  specialization VARCHAR(60) NOT NULL,
   phoneNr VARCHAR(10) NOT NULL,
-  PRIMARY KEY (scientistId)
+  labId INT NOT NULL,
+  PRIMARY KEY (scientistId),
+  FOREIGN KEY (labId) REFERENCES laboratory(labId)
 );
 
 CREATE TABLE medicament
 (
-  medName VARCHAR(20) NOT NULL,
-  medType VARCHAR(20) NOT NULL,
+  medName VARCHAR(60) NOT NULL,
+  medType VARCHAR(60) NOT NULL,
   medId INT NOT NULL,
   quantity INT NOT NULL,
-  factCompany VARCHAR(25) NOT NULL,
+  factCompany VARCHAR(60) NOT NULL,
   PRIMARY KEY (medId)
 );
 
 CREATE TABLE virus
 (
-  virusName VARCHAR(20) NOT NULL,
+  virusName VARCHAR(60) NOT NULL,
   virusId INT NOT NULL,
-  source VARCHAR(25) NOT NULL,
+  sources VARCHAR(60) NOT NULL,
   dangerLevel INT NOT NULL,
   PRIMARY KEY (virusId)
+);
+
+CREATE TABLE analyzes
+(
+  labId INT NOT NULL,
+  virusId INT NOT NULL,
+  PRIMARY KEY (labId, virusId),
+  FOREIGN KEY (labId) REFERENCES laboratory(labId),
+  FOREIGN KEY (virusId) REFERENCES virus(virusId)
+);
+
+CREATE TABLE creates
+(
+  medId INT NOT NULL,
+  labId INT NOT NULL,
+  PRIMARY KEY (medId, labId),
+  FOREIGN KEY (medId) REFERENCES medicament(medId),
+  FOREIGN KEY (labId) REFERENCES laboratory(labId)
 );
 
 CREATE TABLE cure
@@ -44,33 +73,4 @@ CREATE TABLE work_on
   PRIMARY KEY (scientistId, medId),
   FOREIGN KEY (scientistId) REFERENCES scientist(scientistId),
   FOREIGN KEY (medId) REFERENCES medicament(medId)
-);
-
-CREATE TABLE laboratory
-(
-  labLocation VARCHAR(25) NOT NULL,
-  protectionLevel INT NOT NULL,
-  labName VARCHAR(20) NOT NULL,
-  labId INT NOT NULL,
-  scientistId INT NOT NULL,
-  PRIMARY KEY (labId),
-  FOREIGN KEY (scientistId) REFERENCES scientist(scientistId)
-);
-
-CREATE TABLE analyzes
-(
-  labId INT NOT NULL,
-  virusId INT NOT NULL,
-  PRIMARY KEY (labId, virusId),
-  FOREIGN KEY (labId) REFERENCES laboratory(labId),
-  FOREIGN KEY (virusId) REFERENCES virus(virusId)
-);
-
-CREATE TABLE create
-(
-  medId INT NOT NULL,
-  labId INT NOT NULL,
-  PRIMARY KEY (medId, labId),
-  FOREIGN KEY (medId) REFERENCES medicament(medId),
-  FOREIGN KEY (labId) REFERENCES laboratory(labId)
 );
