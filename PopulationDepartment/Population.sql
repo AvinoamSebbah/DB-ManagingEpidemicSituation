c1b3b324dd97c4d0f6e528f929d70bbedec6c9f0
Avinoam Sebbah <45938118+AvinoamSebbah@users.noreply.github.com>
2020-05-24 16:48:27 +0300

    Update Population.sql

--- PopulationDepartment/Population.sql
+++ PopulationDepartment/Population.sql
@@ -1,69 +1,35 @@
 CREATE TABLE City
-
 (
-
   name VARCHAR NOT NULL,
-
   city_id INT NOT NULL,
-
   number_of_residents NUMERIC NOT NULL,
-
   current_state INT NOT NULL,
-
   PRIMARY KEY (city_id)
-
 );
-
-
 
 CREATE TABLE Population_inspection_companies
-
 (
-
   examination_type CHAR NOT NULL,
-
   capacity CHAR NOT NULL,
-
   id INT NOT NULL,
-
   city_id INT NOT NULL,
-
   PRIMARY KEY (id, city_id),
-
   FOREIGN KEY (city_id) REFERENCES City(city_id)
-
 );
 
-
-
 CREATE TABLE people
-
 (
-
   id INT NOT NULL,
-
   first_name VARCHAR(20) NOT NULL,
-
   address VARCHAR(20) NOT NULL,
-
   last_name VARCHAR(20) NOT NULL,
-
   phone_number INT NOT NULL,
-
   Quarantined CHAR NOT NULL,
-
   waiting_examination CHAR NOT NULL,
-
   city_id INT NOT NULL,
-
   pic_id INT NOT NULL,
-
   id INT NOT NULL,
-
   PRIMARY KEY (id),
-
   FOREIGN KEY (city_id) REFERENCES City(city_id),
-
   FOREIGN KEY (pic_id, id) REFERENCES Population_inspection_companies(id, city_id)
-
 );
