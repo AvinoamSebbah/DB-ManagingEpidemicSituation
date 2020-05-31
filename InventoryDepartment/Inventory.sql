bdd8550cbe77d8c22203572470199f75dc3f0676
Avinoam Sebbah <45938118+AvinoamSebbah@users.noreply.github.com>
2020-05-31 19:03:58 +0300

    Update Inventory.sql

--- InventoryDepartment/Inventory.sql
+++ InventoryDepartment/Inventory.sql
@@ -1,3 +1,12 @@
+CREATE TABLE Medical_Inventory
+(
+  Name_Of_Product VARCHAR(20) NOT NULL,
+  Inventory INT NOT NULL,
+  Importer VARCHAR(20) NOT NULL,
+  Product_ID VARCHAR(20) NOT NULL,
+  PRIMARY KEY (Name_Of_Product)
+);
+
 CREATE TABLE Courier
 (
   Company_Name VARCHAR(20) NOT NULL,
@@ -16,60 +25,26 @@ CREATE TABLE Medical_equipment_manufacturing_companies
   UNIQUE (Company_address)
 );
 
-CREATE TABLE Public
-(
-  Name VARCHAR(20) NOT NULL,
-  PRIMARY KEY (Name)
-);
-
-CREATE TABLE supermarket_chain
-(
-  Inventory INT NOT NULL,
-  Area INT NOT NULL,
-  Company_Name VARCHAR(20) NOT NULL,
-  PRIMARY KEY (Company_Name)
-);
-
 CREATE TABLE Hospital
 (
-  Name VARCHAR(20) NOT NULL,
-  PRIMARY KEY (Name)
-);
-
-CREATE TABLE Delivered_to-
-(
-  Company_Name VARCHAR(20) NOT NULL,
-  Name VARCHAR(20) NOT NULL,
-  PRIMARY KEY (Company_Name, Name),
-  FOREIGN KEY (Company_Name) REFERENCES Courier(Company_Name),
-  FOREIGN KEY (Name) REFERENCES Public(Name)
-);
-
-CREATE TABLE Transferred_to-
-(
-  Company_Name VARCHAR(20) NOT NULL,
-  Company_Name VARCHAR(20) NOT NULL,
-  PRIMARY KEY (Company_Name, Company_Name),
-  FOREIGN KEY (Company_Name) REFERENCES Courier(Company_Name),
-  FOREIGN KEY (Company_Name) REFERENCES supermarket_chain(Company_Name)
-);
-
-CREATE TABLE Medical_Inventory
-(
-  Name VARCHAR(20) NOT NULL,
-  Inventory INT NOT NULL,
-  Importer VARCHAR(20) NOT NULL,
-  Product_ID VARCHAR(20) NOT NULL,
-  Name VARCHAR(20) NOT NULL,
-  PRIMARY KEY (Name),
-  FOREIGN KEY (Name) REFERENCES Hospital(Name)
+  Hospital_Name VARCHAR(20) NOT NULL,
+  PRIMARY KEY (Hospital_Name)
 );
 
 CREATE TABLE Manufacture_to-
 (
   Company_Name VARCHAR(20) NOT NULL,
-  Name VARCHAR(20) NOT NULL,
-  PRIMARY KEY (Company_Name, Name),
+  Name_Of_Product VARCHAR(20) NOT NULL,
+  PRIMARY KEY (Company_Name, Name_Of_Product),
   FOREIGN KEY (Company_Name) REFERENCES Medical_equipment_manufacturing_companies(Company_Name),
-  FOREIGN KEY (Name) REFERENCES Medical_Inventory(Name)
+  FOREIGN KEY (Name_Of_Product) REFERENCES Medical_Inventory(Name_Of_Product)
+);
+
+CREATE TABLE Delivered_to-
+(
+  Company_Name VARCHAR(20) NOT NULL,
+  Hospital_Name VARCHAR(20) NOT NULL,
+  PRIMARY KEY (Company_Name, Hospital_Name),
+  FOREIGN KEY (Company_Name) REFERENCES Courier(Company_Name),
+  FOREIGN KEY (Hospital_Name) REFERENCES Hospital(Hospital_Name)
 );
